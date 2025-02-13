040e26953aa4b5fb7412ed0c3acbb699e18bfba0
RodrigoGCorrea <rodrigogc@id.uff.br>
2025-02-13 18:46:24 -0300

    refactor: changing column name to max_weight

--- backend/entity/truck_entity.py
+++ backend/entity/truck_entity.py
@@ -10,6 +10,6 @@ class TruckEntity(Base):
     __tablename__ = "truck"
 
     truck_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    weight = Column(Float, nullable=False)
+    max_weight = Column(Float, nullable=False)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
