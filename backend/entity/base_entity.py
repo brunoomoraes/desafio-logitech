f3048c6bc2247b8d2b3d1c52d2e2bfcc63287abc
RodrigoGCorrea <rodrigogc@id.uff.br>
2025-02-14 14:49:37 -0300

    refactor: adding the created_at and updated_at in the Base class

--- backend/entity/base_entity.py
+++ backend/entity/base_entity.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    pass
+    __abstract__ = True
+
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
