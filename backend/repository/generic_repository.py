6552229a8f2a380f01a73d39e556d30437bc3989
RodrigoGCorrea <rodrigogc@id.uff.br>
2025-02-13 22:36:13 -0300

    refactor: generalizing repository

---
+++ backend/repository/generic_repository.py
@@ -0,0 +1,34 @@
+from typing import TypeVar, Generic, Optional
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
+from entity.base_entity import Base
+
+T = TypeVar("T", bound=Base)
+
+class GenericRepository(Generic[T]):
+    def __init__(self, db: Session, model: Base) -> None:
+        self.db = db
+        self.model = model
+
+    def create(self, entity: T) -> T:
+        try:
+            self.db.add(entity)
+            self.db.commit()
+            self.db.refresh(entity)
+            return entity
+        except Exception as e:
+            self.db.rollback()
+            raise e
+
+    def get_all(self) -> list[T]:
+        return self.db.query(self.model).all()
+
+    def update(self, entity: T) -> T:
+        self.db.refresh(entity)
+        return entity
+
+    def delete(self, entity: T) -> None:
+        self.db.delete(T)
+        self.db.commit()
--- backend/repository/order_repository.py
+++ backend/repository/order_repository.py
@@ -1,4 +1,4 @@
-from typing import Type, Optional
+from typing import Optional
 from uuid import UUID
 
 from fastapi import Depends
@@ -6,37 +6,12 @@ from sqlalchemy.orm import Session
 
 from database.postgres import get_db
 from entity.order_entity import OrderEntity
+from repository.generic_repository import GenericRepository
 
 
-class OrderRepository:
+class OrderRepository(GenericRepository[OrderEntity]):
     def __init__(self, db: Session) -> None:
-        self.db = db
-
-    def create(self, order: OrderEntity) -> OrderEntity:
-        try:
-            self.db.add(order)
-            self.db.commit()
-            self.db.refresh(order)
-            return order
-        except Exception as e:
-            self.db.rollback()
-            raise e
-
-    def find_by_id(self, order_id: UUID) -> Optional[OrderEntity]:
-        return (
-            self.db.query(OrderEntity).filter(OrderEntity.order_id == order_id).first()
-        )
-
-    def get_all(self) -> list[OrderEntity]:
-        return self.db.query(OrderEntity).all()
-
-    def update(self, order: OrderEntity) -> OrderEntity:
-        self.db.refresh(order)
-        return order
-
-    def delete(self, order: OrderEntity) -> None:
-        self.db.delete(order)
-        self.db.commit()
+        super().__init__(db, OrderEntity)
 
 
 def get_order_repository(db: Session = Depends(get_db)) -> OrderRepository:
