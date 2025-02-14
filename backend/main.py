273968198e6a50b13766d4b95e24570a6004c0ee
RodrigoGCorrea <rodrigogc@id.uff.br>
2025-02-13 23:45:31 -0300

    feat: adding truck router

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
 from router.order_router import order_router
+from router.truck_router import truck_router
 
 app = FastAPI(title="LogiTech API")
 
 app.include_router(order_router)
+app.include_router(truck_router)
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
