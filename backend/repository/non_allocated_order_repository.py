093bb271ad61ffd1757c0f777cb05806d7ca06b1
RodrigoGCorrea <rodrigogc@id.uff.br>
2025-02-14 15:28:23 -0300

    feat: adding NonAllocatedOrderRepository

---
+++ backend/repository/non_allocated_order_repository.py
@@ -0,0 +1,14 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from database.postgres import get_db
+from entity import NonAllocatedOrderEntity
+from repository.generic_repository import GenericRepository
+
+
+class NonAllocatedOrderRepository(GenericRepository[NonAllocatedOrderEntity]):
+    def __init__(self, db: Session):
+        super().__init__(db, NonAllocatedOrderEntity)
+
+def get_non_allocated_order_repository(db: Session = Depends(get_db)):
+    return NonAllocatedOrderRepository(db)
