48f002244ec8d6598b9434b959f3b3b24cd0b368
RodrigoGCorrea <rodrigogc@id.uff.br>
2025-02-14 23:16:12 -0300

    feat: adding DistributionController

---
+++ backend/controller/distribution_controller.py
@@ -0,0 +1,20 @@
+from fastapi import Depends
+
+from dto.distribution_response_dto import DistributionResponseDTO
+from service.distribution_service import DistributionService, get_distribution_service
+
+
+class DistributionController:
+    def __init__(self, distribution_service: DistributionService):
+        self.distribution_service = distribution_service
+
+    def distribute_orders(self):
+        return DistributionResponseDTO.from_distribution_order_service_dto(
+            self.distribution_service.distribute_orders()
+        )
+
+
+def get_distribution_controller(
+    distribution_service: DistributionService = Depends(get_distribution_service),
+) -> DistributionController:
+    return DistributionController(distribution_service)
