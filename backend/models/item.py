a024d8f550e38fa55eeac324a6408b269e83dc82
Guilherme Baumgratz Figueiroa <gbaumgratzf@gmail.com>
2025-02-16 18:56:39 -0300

    Feat: create the models

---
+++ backend/models/item.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from backend.database import Base
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    weight = Column(Float)
+    truck_id = Column(Integer, ForeignKey("trucks.id"), nullable=True)
+
+    truck = relationship("Truck", back_populates="items", foreign_keys=[truck_id])
